07b801c081d5d1b60a5d53ab100b5bedf018e16a
Eric <ericfrancis1@proton.me>
2023-09-20 19:40:06 -0500

    new LOCAL_ROOT

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.use('/upload', uploadRouter);
 
 
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
